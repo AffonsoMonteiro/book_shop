ecf83fecdf1ee6644127db546c2018f89dd31f23
AffonsoMonteiro <afonsomonteiro_@hotmail.com>
2021-01-11 23:57:36 -0300

    Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,123 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md'
+ 
+
+import { ListBook } from './styled'
 
 export default function Home() {
     return (
-        <h1>BookShop</h1>
+        <ListBook>
+            <li> 
+                <img src="https://m.media-amazon.com/images/I/61YBONj8-QL._AC_UY218_.jpg" alt="" />
+
+                <strong>O último desejo - The Witche</strong>
+                <span>R$ 41,50</span>
+
+                <button type="button">
+                    <div className="CartIcon">
+                        <MdAddShoppingCart size={16} color="#FFF" />
+                    </div>
+                    <span>Adicionar ao carrinho</span>
+                </button>
+            </li>
+
+            <li> 
+                <img src="https://m.media-amazon.com/images/I/618iEKbcBDL._AC_UY218_.jpg" alt="" />
+
+                <strong>Tempo do desprezo - The Witcher</strong>
+                <span>R$ 45,00</span>
+
+                <button type="button">
+                    <div className="CartIcon">
+                        <MdAddShoppingCart size={16} color="#FFF" />
+                    </div>
+                    <span>Adicionar ao carrinho</span>
+                </button>
+            </li>
+
+            <li> 
+                <img src="https://m.media-amazon.com/images/I/618r42IcwRL._AC_UY218_.jpg" alt="" />
+
+                <strong>Batismo de fogo - The Witcher</strong>
+                <span>R$ 34,90</span>
+
+                <button type="button">
+                    <div className="CartIcon">
+                        <MdAddShoppingCart size={16} color="#FFF" />
+                    </div>
+                    <span>Adicionar ao carrinho</span>
+                </button>
+            </li>
+
+            <li> 
+                <img src="https://m.media-amazon.com/images/I/61ZdevvEQiL._AC_UY218_.jpg" alt="" />
+
+                <strong>A Espada do Destino - The Witcher</strong>
+                <span>R$ 33,90</span>
+
+                <button type="button">
+                    <div className="CartIcon">
+                        <MdAddShoppingCart size={16} color="#FFF" />
+                    </div>
+                    <span>Adicionar ao carrinho</span>
+                </button>
+            </li>
+
+            <li> 
+                <img src="https://m.media-amazon.com/images/I/61iT2u7EGhL._AC_UY218_.jpg" alt="" />
+
+                <strong>O Sangue dos Elfos - The Witcher</strong>
+                <span>R$ 39,90</span>
+
+                <button type="button">
+                    <div className="CartIcon">
+                        <MdAddShoppingCart size={16} color="#FFF" />
+                    </div>
+                    <span>Adicionar ao carrinho</span>
+                </button>
+            </li>
+
+            <li> 
+                <img src="https://m.media-amazon.com/images/I/71QPDt8hYrL._AC_UY218_.jpg" alt="" />
+
+                <strong>A Senhora do lago - The Witcher</strong>
+                <span>R$ 48,90</span>
+
+                <button type="button">
+                    <div className="CartIcon">
+                        <MdAddShoppingCart size={16} color="#FFF" />
+                    </div>
+                    <span>Adicionar ao carrinho</span>
+                </button>
+            </li>
+
+            <li> 
+                <img src="https://m.media-amazon.com/images/I/810Znwbjy3L._AC_UY218_.jpg" alt="" />
+
+                <strong>Fogo & Sangue</strong>
+                <span>R$ 34,90</span>
+
+                <button type="button">
+                    <div className="CartIcon">
+                        <MdAddShoppingCart size={16} color="#FFF" />
+                    </div>
+                    <span>Adicionar ao carrinho</span>
+                </button>
+            </li>
+
+            <li> 
+                <img src="https://m.media-amazon.com/images/I/91PglZzF9kL._AC_UY218_.jpg" alt="" />
+
+                <strong>A fúria dos reis: As Crônicas de Gelo e Fogo</strong>
+                <span>R$ 63,90</span>
+
+                <button type="button">
+                    <div className="CartIcon">
+                        <MdAddShoppingCart size={16} color="#FFF" />
+                    </div>
+                    <span>Adicionar ao carrinho</span>
+                </button>
+            </li>
+        </ListBook>
     )
 }
---
+++ src/pages/Home/styled.js
@@ -0,0 +1,61 @@
+import styled from 'styled-components'
+
+
+export const ListBook = styled.ul ` 
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 20px;
+    list-style: none;
+    margin-top: 10px;
+    
+
+    li { 
+        display: flex;
+        flex-direction: column;
+        background-color: #fff;
+        border-radius: 4px;
+        padding: 20px;
+
+        img {
+            max-width: 250px;   
+            
+            align-self: center;        
+        }
+
+        > strong {
+            font-size: 15px;
+            line-height: 20px;
+            color: #333;
+            margin-top: 5px;
+        }
+
+        > span {
+            font-size: 20px;
+            font-weight: bold;
+            margin: 5px 0 20px;
+        }
+
+        button {
+            background: #573BA3;
+            color: #FFF;
+            border: none;
+            border-radius: 5px;
+            overflow: hidden;
+            margin-top: auto;
+
+            display: flex;
+            align-items: center;
+
+            .CartIcon {
+                display: flex;
+                align-items: center;
+                padding: 12px;
+
+                svg {
+                    margin-right: 5px;
+                }
+            }
+
+        }
+    }
+`
