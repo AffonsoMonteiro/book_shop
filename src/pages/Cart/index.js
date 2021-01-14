437ab576dba76d4287fa55556333c5d8df7f856f
AffonsoMonteiro <afonsomonteiro_@hotmail.com>
2021-01-13 21:46:41 -0300

    create page Cart
    criata a pagina do carrinho mas sem funcionalidades

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,17 +1,65 @@
 import React from 'react';
+import {MdRemoveCircleOutline, MdAddCircleOutline, MdDelete} from 'react-icons/md'
+import ReactTooltip from 'react-tooltip';
+
 
 import { Container, CardProduct } from './styled'
 
 export default function Cart() {
     return (
         <Container>
-            <CardProduct>
-            
+            <CardProduct >
+                <thead>
+                    <tr>
+                        < th />
+                        <th>Produto</th>
+                        <th>Quantidade</th>
+                        <th>Preço</th>
+                        <th />
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr >
+                        <td>
+                            <img src="https://m.media-amazon.com/images/I/61YBONj8-QL._AC_UY218_.jpg" alt="Livro" />
+                        </td>
+                        <td>
+                            <div className="nameProduct">O último desejo - The Witche</div>
+                            <div className="priceProduct">R$ 34,90 </div>
+                        </td>
+                        <td >
+                            <div className="qtdButton">
+                                <button type="button">
+                                    <MdRemoveCircleOutline size={20} />
+                                </button>
+                                <input type="number" readOnly value={2} />
+                                <button type="button">
+                                    <MdAddCircleOutline size={20} />
+                                </button>
+                            </div>
+                        </td>
+                        <td>
+                            <strong  >R$ 69,80</strong>
+                        </td>
+                        <td  >
+                            <button type="button" className="btnRemove" data-tip="Excluir"  > 
+                                <MdDelete size={20}  />
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
             </CardProduct>
 
             <footer>
-                <button></button>
+                <button type="button" >Finalizar pedido</button>
+
+                <div className="total">
+                    <span>Total</span>
+                    <strong> R$ 34,90 </strong>
+                </div>
             </footer>
+      <ReactTooltip place="top" effect="solid" />
+
         </Container>
     )
 }
--- src/pages/Cart/styled.js
+++ src/pages/Cart/styled.js
@@ -2,8 +2,107 @@ import styled from 'styled-components'
 
 
 export const Container = styled.div `
-    
+    padding: 30px;
+    background: #FFF;
+    margin: 20px 100px;
+    border-radius: 5px;
+
+
+    footer {
+        margin-top: 30px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        button {
+            background: #573BA3;
+            color: #FFF;
+            font-weight: bold;
+            border: none;
+            border-radius: 5px;
+            padding: 15px 50px;
+            text-transform: uppercase;
+            transition: all ease .3s;
+
+            &:hover {
+                opacity: 0.9;
+            }
+        }
+
+        .total {
+            display: flex;
+            align-items: baseline;
+
+            span{
+                color: #888;
+                font-weight: bold;
+            }
+
+            strong {
+                font-size: 26px;
+                margin-left: 5px;
+            }
+        }
+    }
 `
 
 
-export const CardProduct = styled.div `` 
+export const CardProduct = styled.table `
+    width: 100%;
+
+    thead th {
+        color: #888;
+        text-align: left;
+
+        padding: 15px;
+    }
+
+    tbody td {
+        padding: 15px;
+        border-bottom: 1px solid #ccc;
+    }
+
+    img {
+        height:150px;
+        
+    }
+
+    .nameProduct {
+        color: #333;
+        display: block
+    }
+
+    .priceProduct {
+        margin-top: 5px;
+        font-size: 18px;
+        font-weight: bold;
+        display: block
+    }
+
+    .qtdButton {
+        display: flex;
+        align-items: center;
+
+        input {
+            border: 1px solid #CCC;
+            border-radius: 4px;
+            color: #677;
+            width: 50px;
+            padding: 6px;
+            text-align: center;
+        }
+
+        button {
+            background: none;
+            border: none;
+            padding: 5px;
+        }
+    }
+
+    .btnRemove {
+        border: none;
+        background: none;
+
+       
+    }
+` 
